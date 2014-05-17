b24f715ef534d99d9445f572d6b7b9be39d1d5cd
Marc O'Morain <marc.omorain@swrve.com>
2014-05-17 12:29:55 +0100

    Initial Commit

---
+++ GameCraftParty/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  GameCraftParty
+//
+
+//  Copyright (c) 2014 Marc. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ GameCraftParty/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GameCraftParty
+//
+//  Created by Marc O'Morain on 17/05/2014.
+//  Copyright (c) 2014 Marc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
